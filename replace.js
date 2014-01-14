e0c4b38d1be5d5af76395ff83fcdbf4c8ad36187
rankun203 <rankun203@gmail.com>
2014-01-14 16:21:33 +0800

    添加图标

--- replace.js
+++ replace.js
@@ -1,19 +1,25 @@
+var imgsCount = 0;//cache image count
+
 function doReplace() {
-  console.log("Start replace static emotions.");
   var start = new Date().getTime();
   var imgs = document.getElementsByTagName("img");
-  for(var i = 0, j = imgs.length; i < j; i++) {
-    var img = imgs[i];
-    var src = img.src;
-    if(typeof src !== 'undefined') {
-      if(src.indexOf("http://qzonestyle.gtimg.cn/qzone/em/") > -1) {
-        img.src = "http://ctc.qzs.qq.com/qzone/em/" + src.match(/\e[\d]+/) + ".gif";
-        continue;
-      } else if (src.indexOf("http://ctc.qzonestyle.gtimg.cn/qzone/em/") > -1) {
-        img.src = "http://ctc.qzs.qq.com/qzone/em/" + src.match(/\e[\d]+/) + ".gif";
+  var _imgsCount = imgs.length;
+  if(_imgsCount > imgsCount){
+    for(var i = 0, j = imgs.length; i < j; i++) {
+      var img = imgs[i];
+      var src = img.src;
+      if(typeof src !== 'undefined') {
+        if(src.indexOf("http://qzonestyle.gtimg.cn/qzone/em/") > -1) {
+          img.src = "http://ctc.qzs.qq.com/qzone/em/" + src.match(/\e[\d]+/) + ".gif";
+          continue;
+        } else if (src.indexOf("http://ctc.qzonestyle.gtimg.cn/qzone/em/") > -1) {
+          img.src = "http://ctc.qzs.qq.com/qzone/em/" + src.match(/\e[\d]+/) + ".gif";
+        }
       }
+
     }
+
   }
-  console.log("Replace takes: " + ((new Date().getTime()) - start) + "ms");
+
 }
-setInterval(function(){doReplace();}, 5000);
+setInterval(function(){doReplace();}, 1000);
